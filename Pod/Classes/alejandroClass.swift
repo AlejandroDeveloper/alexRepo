0400b5e1e53f1aeb0a840c2f0d28aa3634b31a9b
AlejandroDeveloper <alejandro.palomo@external.grifols.com>
2016-04-14 12:49:06 +0200

    Segundo commit

---
+++ Pod/Classes/alejandroClass.swift
@@ -0,0 +1,16 @@
+//
+//  alejandroClass.swift
+//  Pods
+//
+//  Created by Alejandro Palomo Rodriguez on 14/4/16.
+//
+//
+
+import Foundation
+
+public class alejandroClass {
+    
+    public func showName(){
+        print("El nombre es alejandro")
+    }
+}
